c70ef47b129bebb270afee9c133c560f22a2cf98
elizafischer <efischer@fwparker.org>
2017-02-25 13:23:49 -0600

    done but binary search is not working completely right

--- ch15Lab.py
+++ ch15Lab.py
@@ -31,6 +31,7 @@ for line in file:
 
 file.close()
 '''
+#############################################
 file = open("AliceInWonderLand200.txt", "r")
 for line in file:
     words = split_line(line)
@@ -46,9 +47,57 @@ if i < len(name_list):
     print("The name is at position", i)
 else:
     print("The name was not in the list.")
+#############################################
 '''
 
-print("**--- Binary Search ---**")
-# LOOK AT FEB 7 AND 10
+print("\n**--- Binary Search ---**")
+# LOOK AT NOTES FROM FEB 7 AND 10
 
+file = open("AliceInWonderLand200.txt")
 line_num = 0
+upper_limit = len(dictionary_list) - 1
+lower_limit = 0
+found = False
+
+for line in file:
+    words = split_line(line)
+    line_num += 1
+    for word in words:
+        while lower_limit <= upper_limit and not found:
+            middle_position = (lower_limit + upper_limit) // 2
+
+            if dictionary_list[middle_position].upper() > word.upper():
+                upper_limit -= middle_position - 1
+
+            elif dictionary_list[middle_position].upper() < word.upper():
+                lower_limit += middle_position + 1
+            else:
+                found = True
+        if not found:
+            print("Check", word, "'s spelling at line", line_num)
+    found = False
+    upper_limit = len(dictionary_list) - 1
+    lower_limit = 0
+file.close()
+
+
+'''
+for line in file:
+    words = split_line(line)
+    line_num += 1
+    for word in words:
+        key = word
+        while lower_limit <= upper_limit and not found:
+            middle_pos = (upper_limit + lower_limit) //2
+
+            if dictionary_list[middle_pos] > key.upper():
+                upper_limit = middle_pos + 1
+
+            elif dictionary_list[middle_pos] < key.upper():
+                lower_limit = middle_pos + 1
+
+            else:
+                found = True
+        if not found:
+            print("Check:", key, "'s spelling at line", line_num)
+'''
